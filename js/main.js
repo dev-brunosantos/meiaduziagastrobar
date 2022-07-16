d71abfbd953b1f1278a690f5e57498d0969338e3
dev-brunosantos <100520104+dev-brunosantos@users.noreply.github.com>
2022-07-16 16:02:26 -0300

    Primeira versão do código

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// TELA DE LOGIN DE FUNCIONÁRIOS
+
+/*var tela = document.querySelector('.login');
+
+var usuario = document.querySelector('#funcionarios');
+var senha;
+
+const botaoIniciar = document.querySelector('#btnLogin');
+botaoIniciar.addEventListener('click', usuarioIniciar);
+
+function usuarioIniciar() {
+
+    var usuarioNome = usuario.value;
+    console.log(usuarioNome);
+
+    if(usuarioNome == "Bruno") {
+        tela.style.backgroundColor = "blue"
+    }
+    else if(usuarioNome == "Mônica") {
+        tela.style.backgroundColor = "pink"
+    }
+    else if(usuarioNome == "Lucas") {
+        tela.style.backgroundColor = "green"
+    }
+    else {
+        alert("Usuário Inválido")
+    }
+
+}
+*/
